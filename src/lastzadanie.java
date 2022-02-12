c13bcd9f2e290966a9ae379bef4bb74e130c7723
kir9kan <95570841+kir9kan@users.noreply.github.com>
2022-02-12 17:43:19 +0300

    переделка2

--- src/lastzadanie.java
+++ src/lastzadanie.java
@@ -1,12 +1,10 @@
 public class lastzadanie {
     public static void main(String[] args) {
-        int a=22;
+        int a=12;
         if ( a==1 ){
             System.out.println(a + " программист");
         }
-        if (a ==12){
-            System.out.println(a+ " программистов");
-        }
+
         if(a!=12 & a%10>=2 & a%10<5 ){
             System.out.println(a + " программиста");
         }
